9089b8c2a407470162861902c476dbee10b0c75c
Ohnitiel <leitinho.ricardo@gmail.com>
2024-08-13 11:03:39 -0300

    Added starting TUI with bubbletea list

--- main.go
+++ main.go
@@ -4,18 +4,53 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+const listHeight = 15
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(4).Foreground(lipgloss.Color("170"))
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
 }
 
 type Activities struct {
@@ -159,11 +194,10 @@ func fillHours(requestToken string, holidays map[time.Time]bool) {
 	}
 }
 
-func initialModel() model {
-	return model{
-		choices:  []string{"Utilizar usuário e senha", "Utilizar token"},
-		selected: make(map[int]struct{}),
-	}
+type model struct {
+	list     list.Model
+	choice   string
+	quitting bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -172,59 +206,65 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+
 	case tea.KeyMsg:
 
 		switch msg.String() {
 		case "ctrl+c", "q":
+			m.quitting = true
 			return m, tea.Quit
 
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
 			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+				m.choice = string(i)
 			}
+			return m, tea.Quit
 		}
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-        s := "Como deseja utilizar o programa:\n\n"
-
-	for i, choice := range m.choices {
-                cursor := " "
-                if m.cursor == i {
-                        cursor = ">"
-                }
-
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
+	if m.choice != "" {
+		if m.list.Index() == 0 {
+			return quitTextStyle.Render("Username password")
+		} else if m.list.Index() == 1 {
+			return quitTextStyle.Render("Token")
 		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	if m.quitting {
+		return quitTextStyle.Render("Exit")
 	}
 
-	s += "\n Para sair, pressione Ctrl+C ou Q"
-
-	return s
+	return "\n" + m.list.View()
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	items := []list.Item{
+		item("Login com usuário e senha"),
+		item("Login com token"),
+	}
+
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "Como deseja realizar o login?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.HelpStyle = helpStyle
+
+	m := model{list: l}
+
+	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Algo deu errado: %v\n", err)
 		os.Exit(1)
